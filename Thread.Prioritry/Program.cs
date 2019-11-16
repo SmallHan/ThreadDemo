2cf397b6c1cbc668727a6c51517f3e80e1703296
SmallHan <316598703@qq.com>
2019-11-16 15:31:56 +0800

    线程学习

---
+++ Thread.Prioritry/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPrioritry
+{
+    /// <summary>
+    /// 线程优先级
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current thread  priority:{Thread.CurrentThread.Priority}");
+            Console.WriteLine($"Running on all cores available");
+            RunThreads();
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine($"Running on a single core");
+            Console.ReadKey();
+        }
+
+        static void RunThreads()
+        {
+            var sample = new ThreadSample();
+
+            var threadOne = new Thread(sample.CountNumbers);
+            threadOne.Name = "ThreadOne";
+            var threadTwo = new Thread(sample.CountNumbers);
+            threadTwo.Name = "ThreadTwo";
+
+            //设置线程优先级
+            threadOne.Priority = ThreadPriority.Highest;
+            threadTwo.Priority = ThreadPriority.Lowest;
+
+            threadOne.Start();
+            threadTwo.Start();
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            sample.Stop();
+        }
+    }
+    class ThreadSample
+    {
+        private bool _isStopped = false;
+
+        public void Stop()
+        {
+            _isStopped = true;
+        }
+        public void CountNumbers()
+        {
+            
+            long counter = 0;
+            while (!_isStopped)
+            {
+                counter++;
+            }
+            Console.WriteLine($"{Thread.CurrentThread.Name}" +
+                $"{Thread.CurrentThread.Priority,11} priority" +
+                $"has a count={counter}");
+        }
+    }
+}
---
+++ ThreadPrams/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPrams
+{
+    /// <summary>
+    /// 向线程传递参数
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadSample sample = new ThreadSample(10);
+            Thread threadOne = new Thread(sample.CountNumbers);
+            threadOne.Name = "ThreadOne";
+            threadOne.Start();
+            threadOne.Join();
+            Console.WriteLine("-------华丽得分割线-------");
+
+            Thread threadTwo = new Thread(Count);
+            threadTwo.Name = "ThreadTwo";
+            threadTwo.Start(8);
+            threadTwo.Join();
+            Console.WriteLine("-------华丽得分割线-------");
+
+            Thread threadThree = new Thread(()=> CountNumbers(12));
+            threadThree.Name = "ThreadThree";
+            threadThree.Start();
+            threadThree.Join();
+            Console.WriteLine("-------华丽得分割线-------");
+
+            //打印出20，因为在执行线程前值已经改为20了
+            int i = 10;
+            var threadFour = new Thread(()=> PrintNumber(i));
+            i = 20;
+            var threadFive = new Thread(() => PrintNumber(i));
+            threadFour.Start();
+            threadFive.Start();
+            Console.ReadKey();
+        }
+
+        static void Count(object iterations)
+        {
+            CountNumbers((int)iterations);
+        }
+        static void CountNumbers(int iterations)
+        {
+            for (var i = 1; i < iterations; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine($"{Thread.CurrentThread.Name} prints {i}");
+            }
+        }
+        static void PrintNumber(int number)
+        {
+            Console.WriteLine(number);
+        }
+    }
+    class ThreadSample
+    {
+        private readonly int _iterations;
+        public ThreadSample(int iterations)
+        {
+            _iterations = iterations;
+        }
+        public void CountNumbers()
+        {
+            for (var i = 1; i < _iterations; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine($"{Thread.CurrentThread.Name} prints {i}");
+            }
+        }
+    }
+}
