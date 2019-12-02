e6202a36236f429c3a212d81e7f71f652ed63e28
SmallHan <316598703@qq.com>
2019-12-02 22:59:45 +0800

    对并行执行的异步操作使用await操作符

--- Await/Program.cs
+++ Await/Program.cs
@@ -30,16 +30,58 @@ namespace AwaitResult
         static async Task<string> GetInfoAsync(string name)
         {
             await Task.Delay(TimeSpan.FromSeconds(2));
-            throw new Exception("Boom!!");
+            //throw new Exception("Boom!!");
             return $"Task {name} is running on a thread id{Thread.CurrentThread.ManagedThreadId}.Is" +
                 $" thread pool thread:{Thread.CurrentThread.IsThreadPoolThread}";
         }
-        static void Main(string[] args)
-        {
-            Task t = AsynchronyWithTPL();
-            t.Wait();
 
-            t = AsynchronyWithAwait();
+        static async Task AsynchronousProcessing()
+        {
+            Func<string, Task<string>> asyncLambda = async name =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return $"Task {name} is running on a thread id{Thread.CurrentThread.ManagedThreadId}.Is" +
+               $" thread pool thread:{Thread.CurrentThread.IsThreadPoolThread}";
+            };
+            string result = await asyncLambda("async Lambda");
+            Console.WriteLine(result);
+        }
+
+        static async Task AsynchronyWithAwait1()
+        {
+            try
+            {
+                string result = await GetInfoAsync1("Async 1");
+                Console.WriteLine(result);
+                result = await GetInfoAsync1("Async 2");
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        static async Task<string> GetInfoAsync1(string name)
+        {
+            Console.WriteLine($"Task {name} started!");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            return $"Task {name} is running on a thread id{Thread.CurrentThread.ManagedThreadId}.Is" +
+            $" thread pool thread:{Thread.CurrentThread.IsThreadPoolThread}";
+        }
+        static  void Main(string[] args)
+        {
+            
+            //Task t = AsynchronyWithTPL();
+            //t.Wait();
+
+            //t = AsynchronyWithAwait();
+            //t.Wait();
+
+            //Task t= AsynchronousProcessing();
+            //t.Wait();
+
+            Task t=AsynchronyWithAwait1();
+            Console.WriteLine("666");
             t.Wait();
             Console.ReadKey();
         }
